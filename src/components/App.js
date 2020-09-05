e0528af19648c28f0b03c80695023f7d27e773d9
urielquevedo <quevedouriel3@gmail.com>
2020-09-05 04:28:28 -0300

    Visualizacion de todas las recetas

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getAllRecipes } from '../service/RecipeService';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import { GridListTileBar, IconButton, Container } from '@material-ui/core';
+import InfoIcon from '@material-ui/icons/Info';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+  title: {
+    color: 'red',
+  }
+}));
 
 function App() {
+
+  const classes = useStyles();
+  const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
-    getAllRecipes().then((r) => console.log(r));
+    getAllRecipes().then((r) => setRecipes(r));
   }, []);
 
-  return <div>Hola</div>;
+  return (
+    <Container className={classes.root}>
+      <GridList cellHeight={180} className={classes.gridList}>
+        {recipes.map(({ imageUrl, name }, i) => (
+          <GridListTile key={i}>
+            <img src={imageUrl} alt={name} />
+            <GridListTileBar
+              title={name}
+              actionIcon={
+                <IconButton aria-label={name} className={classes.icon}>
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </Container>
+  );
 }
 
 export default App;
