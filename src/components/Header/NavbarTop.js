fa66015611461276894f60328789947c7c233656
urielquevedo <quevedouriel3@gmail.com>
2020-09-15 20:05:58 -0300

    feat: cambio el estilo y agrego items para navegar en el head

--- src/components/Header/NavbarTop.js
+++ src/components/Header/NavbarTop.js
@@ -1,39 +1,61 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from 'react-router-dom';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import { Link, NavLink } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
+import './NavbarTop.css';
 
 const NavbarTop = () => {
-  const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="sticky" className="navbar-color">
       <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          <Link to='/' style={{ textDecoration: 'none', color: '#fff' }}>Inicio</Link>
-        </Typography>
+        <Grid
+          contianer
+          justify="space-between"
+          alignItems="center"
+          className="layout-display"
+        >
+          <Grid container item xs={6}>
+            <Link to='/'>
+              <img src="/banner.png" alt="banner de la pagina" width="200" />
+            </Link>
+          </Grid>
+          <Grid container item xs={6} justify="flex-end">
+            <Typography variant="h7">
+              <NavLink
+                activeClassName="navbar-item-selected"
+                exact
+                to="/"
+                className="navbar-item"
+              >
+                inicio
+              </NavLink>
+            </Typography>
+            <Typography variant="h7">
+              <NavLink
+                activeClassName="navbar-item-selected"
+                exact
+                to="/recipes/new"
+                className="navbar-item"
+              >
+                crear
+              </NavLink>
+            </Typography>
+            <Typography variant="h7">
+              <NavLink
+                activeClassName="navbar-item-selected"
+                to="/profile"
+                className="navbar-item"
+              >
+                perfil
+              </NavLink>
+            </Typography>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default NavbarTop;
