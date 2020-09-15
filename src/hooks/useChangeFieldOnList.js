2ae1e0b3dca3afc826ea2298ebe6da1cdea2c704
urielquevedo <quevedouriel3@gmail.com>
2020-09-14 22:13:06 -0300

    feat: custom hook para manejar el estado en listas

---
+++ src/hooks/useChangeFieldOnList.js
@@ -0,0 +1,28 @@
+export const useChangeFieldOnList = (customList, setCustomList) => {
+
+  const handleChangeField = (i) => (event) => {
+    const { value, name } = event.target;
+    const custom = customList[i];
+    custom[name] = value;
+    setCustomList((customs_) =>
+    customs_.map((other, idx) => {
+        if (idx === i) return custom;
+        return other;
+      })
+    );
+  };
+
+  const handleNew = () => {
+    setCustomList((customs) => [
+      ...customs,
+      {}
+    ]);
+  }
+
+  const removeOnList = (i) => () => {
+    if(customList.length <= 1) return;
+    setCustomList(customs => customs.filter((_, inx) => inx !== i));
+  }
+
+  return [handleChangeField, handleNew, removeOnList];
+}
