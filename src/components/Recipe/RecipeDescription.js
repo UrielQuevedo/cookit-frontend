eda1a61514adcb3015fd3c0f167a8dcc92149e0f
urielquevedo <quevedouriel3@gmail.com>
2020-09-19 20:51:58 -0300

    feat: creo los componentes para la recipe view

---
+++ src/components/Recipe/RecipeDescription.js
@@ -0,0 +1,36 @@
+import { Grid, Typography } from '@material-ui/core';
+import React from 'react';
+
+const RecipeDescription = ({ imageUrl, name, description }) => {
+  return (
+    <>
+      <Grid container justify="center" item xs={12}>
+        <img className="image-recipe" src={imageUrl} alt={name} />
+      </Grid>
+      <Grid item xs={12} className="plr-20">
+        <Typography
+          variant="h4"
+          className="title-recipe"
+        >
+          {name}
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          className="mb-20 recipe-category"
+        >
+          (categoria)
+        </Typography>
+      </Grid>
+      <Grid item xs={12} className="plr-20">
+        <Typography
+          variant="body1"
+          className="recipe-description"
+        >
+          {description}
+        </Typography>
+      </Grid>
+    </>
+  );
+};
+
+export default RecipeDescription;
