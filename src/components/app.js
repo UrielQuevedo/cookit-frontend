923e81226753c65f243b7849e5c557e05a6458dd
Uriel Quevedo <uquevedo@atixlabs.com>
2020-09-25 17:40:40 -0300

    feat(fix): saco codigo que quedo del conflicto

--- src/components/app.js
+++ src/components/app.js
@@ -3,25 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from 'views/home';
 import New from 'views/Recipes/New/new';
 import NavbarTop from './Header/navbar-top';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Search from '../views/Recipes/Search/search';
-=======
-import Search from '../views/Search/search';
->>>>>>> feat: se agregan los proptypes
-=======
-import Search from '../views/Recipes/Search/search';
->>>>>>> feat(fix): Sacando los warnings
-import Login from '../views/Login/login';
-import Recipe from '../views/Recipes/recipe';
-import AuthProvider from '../context/auth-context';
-=======
 import Search from 'views/Recipes/Search/search';
 import Login from 'views/Login/login';
 import Recipe from 'views/Recipes/recipe';
 import AuthProvider from 'context/auth-context';
->>>>>>> feat: cambio el path de los imports
 
 const App = () => (
   <Router>
