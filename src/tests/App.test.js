841b90f9129485e3ea348fafb23a4b4d26d88835
Dani Villegas <dani.villegascarp@gmail.com>
2020-10-04 14:41:49 -0300

    Se agrega dependencia enzyme y se agrega test de app

---
+++ src/tests/App.test.js
@@ -0,0 +1,11 @@
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
+
+import App from '../components/App'
+
+configure({ adapter: new Adapter() })
+
+it('App levanta', () => {
+  shallow(<App />)
+})
