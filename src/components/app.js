0ba52fd0aa7b1635ae09e9b84a2ed088eafd7798
Dani Villegas <dani.villegascarp@gmail.com>
2020-10-25 21:24:20 -0300

    Se agrega pag inicial de register

--- src/components/app.js
+++ src/components/app.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from 'views/Login/login';
 import AuthProvider from 'context/auth-context';
 import DefaultLayout from './Layout/layout-default';
+import Register from 'views/Register/register';
 
 const App = () => (
   <Router>
     <AuthProvider>
       <Switch>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
         <Route path="/" component={DefaultLayout} />
       </Switch>
     </AuthProvider>
