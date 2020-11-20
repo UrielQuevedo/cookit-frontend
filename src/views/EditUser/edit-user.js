b0c45228814e2630861ef4e9f9db0b2899f890a6
Dani Villegas <dani.villegascarp@gmail.com>
2020-11-20 14:03:33 -0300

    Upload image from user storage

--- src/views/EditUser/edit-user.js
+++ src/views/EditUser/edit-user.js
@@ -4,11 +4,11 @@ import { useHistory } from 'react-router-dom';
 import { me } from 'service/auth-service';
 import { putEditUser } from 'service/user-service';
 import { Button, CircularProgress, Divider, Grid, TextField, InputAdornment, IconButton } from '@material-ui/core';
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import DataContainerProfile from './data-container-profile';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import AvatarImageSelect from 'components/User/avatar-image-select';
 
 const EditUser = () => {
   const { user, setUser } = useContext(UserContext);
@@ -18,7 +18,7 @@ const EditUser = () => {
       email: data.email,
       name: data.name,
       lastname: data.lastname,
-      imageUrl: data.imageUrl,
+      imageUrl: null,
       currentPassword: null,
       newPassword: null
     }
@@ -27,7 +27,8 @@ const EditUser = () => {
   const [userToEdit, setUserToEdit] = useState(defaultValuesToEdit(user));
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState('');
-  const [ loading, setLoading  ] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [files, setFiles] = useState([]);
   const { push } = useHistory();
 
   useEffect(() => {
@@ -44,6 +45,7 @@ const EditUser = () => {
   const sendEditUser = async (event) => {
     event.preventDefault();
     setLoading(true);
+    console.log(userToEdit);
 
     try {
       const _ = await putEditUser(userToEdit);
@@ -102,57 +104,72 @@ const EditUser = () => {
     );
   }
 
-  const settingView = () => {
-    return (
-      <Grid container item justify="center" style={{ padding: '10px' }}>
-        <Grid container item justify="center" className="container-form" direction="column">
-          <form onSubmit={sendEditUser}>
-            {customInput("Email", "email", "email")}
-            {customInput("Nombre", "text", "name")}
-            {customInput("Apellido", "text", "lastname")}
-            {customInput("Imagen", "text", "imageUrl")}
-            {!user.isGoogleAccount && customInput("Nueva Contraseña", "password", "newPassword", "Ingresar contraseña")}
-            {!user.isGoogleAccount &&
-              <div>
-                <Divider style={{ background: '#ffff' }} variant="middle" />
-                <div>
-                  <p style={{ fontSize: '18px', color: '#ffff' }}>
-                    Para confirmar los cambios debe ingresar la contraseña actual
-                  </p>
-                  <TextField
-                    type={showPassword ? "text" : "password"}
-                    required size="small"
-                    className="form-item"
-                    variant="outlined"
-                    placeholder="Ingresar contraseña actual"
-                    onChange={(e) => handleChangeUserValue("currentPassword", e.target.value)}
-                    InputProps={inputPropsShowPassword()}
-                  />
-                </div>
-              </div>
-            }
-            <div>
-              {error && <div style={{ color: '#ffff', display: 'flex', borderLeft: '3px solid #ffff' }}> <HighlightOffIcon style={{ color: '#ffff', margin: '0 5px 0 5px' }} />  {error}. </div>}
-            </div>
-            <Grid container item justify="flex-end" style={{ marginBottom: '10px' }}>
-              { loading && <CircularProgress size={28} style={{ color:'#ffff', marginRight:'15px', position:'relative', top:'3px' }} /> }
-              <Button type="submit" variant="contained" color="primary">
-                Aceptar
-              </Button>
-            </Grid>
-          </form>
-        </Grid>
-      </Grid>
-    );
-  }
 
+  const readFile = () => {
+    var file = document.querySelector('input[type=file]')['files'][0];
+    var reader = new FileReader();
+    var baseString;
+    reader.onloadend = function () {
+        baseString = reader.result;
+        handleChangeUserValue("imageUrl", baseString);
+    };
+    reader.readAsDataURL(file);
+}
+
+const settingView = () => {
   return (
-    <Grid container justify="center" direction="row" style={{ marginTop: '2.5rem', display: 'flex', marginBottom: '30px' }}>
-      <Grid container item xs={9} justify="center">
-        <DataContainerProfile title="Información personal" view={settingView} />
+    <Grid container item justify="center" style={{ padding: '10px' }}>
+      <Grid container item justify="center" className="container-form" direction="column">
+        <Grid item xs={12} justify="center">          
+         <AvatarImageSelect imageUrl={user.imageUrl} name={user.name} lastname={user.lastname} handleChangeUserValue={handleChangeUserValue}/>
+        </Grid>
+        <form onSubmit={sendEditUser}>
+          {customInput("Email", "email", "email")}
+          {customInput("Nombre", "text", "name")}
+          {customInput("Apellido", "text", "lastname")}
+
+          {!user.isGoogleAccount && customInput("Nueva Contraseña", "password", "newPassword", "Ingresar contraseña")}
+          {!user.isGoogleAccount &&
+            <div>
+              <Divider style={{ background: '#ffff' }} variant="middle" />
+              <div>
+                <p style={{ fontSize: '18px', color: '#ffff' }}>
+                  Para confirmar los cambios debe ingresar la contraseña actual
+                  </p>
+                <TextField
+                  type={showPassword ? "text" : "password"}
+                  required size="small"
+                  className="form-item"
+                  variant="outlined"
+                  placeholder="Ingresar contraseña actual"
+                  onChange={(e) => handleChangeUserValue("currentPassword", e.target.value)}
+                  InputProps={inputPropsShowPassword()}
+                />
+              </div>
+            </div>
+          }
+          <div>
+            {error && <div style={{ color: '#ffff', display: 'flex', borderLeft: '3px solid #ffff' }}> <HighlightOffIcon style={{ color: '#ffff', margin: '0 5px 0 5px' }} />  {error}. </div>}
+          </div>
+          <Grid container item justify="flex-end" style={{ marginBottom: '10px' }}>
+            {loading && <CircularProgress size={28} style={{ color: '#ffff', marginRight: '15px', position: 'relative', top: '3px' }} />}
+            <Button type="submit" variant="contained" color="primary">
+              Aceptar
+              </Button>
+          </Grid>
+        </form>
       </Grid>
     </Grid>
-  )
+  );
+}
+
+return (
+  <Grid container justify="center" direction="row" style={{ marginTop: '2.5rem', display: 'flex', marginBottom: '30px' }}>
+    <Grid container item xs={9} justify="center">
+      <DataContainerProfile title="Información personal" view={settingView} />
+    </Grid>
+  </Grid>
+)
 };
 
 export default EditUser;
